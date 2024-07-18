c14f79fe1e529e47803dd2ac3a7f802f215ab666
MatteoBenvenuti <147080848+MatteoBenvenuti@users.noreply.github.com>
2024-07-18 19:39:47 +0200

    Update clone_template.bat

--- clone_template.bat
+++ clone_template.bat
@@ -1,16 +1,34 @@
 @echo off
 
-:: URL della repository da clonare
-set REPO_URL=https://github.com/MatteoBenvenuti/Template-python.git
+:: Disattiva l'eco dei comandi per un aspetto più pulito
+echo.
 
-:: Clona la repository nella directory corrente
-git clone %REPO_URL% .
+:: Chiedi all'utente di inserire il nome della cartella
+set /p FOLDER_NAME=Inserisci il nome della cartella in cui clonare il repository: 
+
+:: URL della repository da clonare
+set REPO_URL=https://github.com/MatteoBenvenuti/Template-python.git 
+
+:: Clona la repository nella directory specificata dall'utente
+echo Clonazione del template in corso...
+git clone %REPO_URL% "./%FOLDER_NAME%" >nul 2>&1
+
+:: Verifica se il comando git clone è riuscito
+if errorlevel 1 (
+    echo Errore durante la clonazione del template. Verifica l'URL e riprova.
+    pause
+    exit /b 1
+)
 
 :: Rimuovi la directory .git
-rmdir /s /q .git
+rmdir /s /q "%FOLDER_NAME%\.git" >nul 2>&1
 
-:: Rimuovi se stesso
+:: Rimuovi il file clone_template.bat dalla cartella clonata
+del "%FOLDER_NAME%\clone_template.bat" >nul 2>&1
+
+@REM :: Rimuovi se stesso
 del "%~f0"
 
-echo Progetto creato nella directory corrente senza la directory .git
+:: Messaggio di log finale
+echo Template clonato con successo nella directory: %FOLDER_NAME%
 pause
